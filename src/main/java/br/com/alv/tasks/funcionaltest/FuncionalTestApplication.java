e80dbfe6d9c3f685b235b4adafbde2b1f3d5a636
Alvaro Melo <alvarolmelo@gmail.com>
2023-07-11 13:06:22 -0300

    first commit

---
+++ src/main/java/br/com/alv/tasks/funcionaltest/FuncionalTestApplication.java
@@ -0,0 +1,13 @@
+package br.com.alv.tasks.funcionaltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuncionalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuncionalTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alv/tasks/funcionaltest/FuncionalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alv.tasks.funcionaltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuncionalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
